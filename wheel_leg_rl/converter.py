7c4669dae85c2f5761858f8eb09e35fdf6e75390
Yao-Xinchen <yao29@illinois.edu>
2024-07-14 18:44:53 +0800

    optimize leg motor convertion

--- wheel_leg_rl/converter.py
+++ wheel_leg_rl/converter.py
@@ -14,37 +14,46 @@ class Converter:
         # pos
         leg_action_pos = self._clip_leg(leg_action_pos)
         leg_real_pos = leg_action_pos + self._leg_offset
-        motor_real_pos = self._leg_to_motor_real(leg_real_pos)
+        motor_real_pos = self._m(leg_real_pos)
         motor_simu_pos = (motor_real_pos - self._motor_offset) * np.array([-1, 1])
         return motor_simu_pos
     
     def motor_to_leg(self, motor_fb_pos, motor_fb_vel):
         # pos
         motor_real_pos = motor_fb_pos * np.array([-1, 1]) + self._motor_offset
-        leg_real_pos = self._motor_to_leg_real(motor_real_pos)
+        leg_real_pos = self._l(motor_real_pos)
         leg_simu_pos = leg_real_pos - self._leg_offset
         # vel
-        temp_dt = 0.001
-        motor_real_pos_next = (motor_fb_pos + motor_fb_vel * temp_dt) * np.array([-1, 1]) + self._motor_offset
-        leg_real_pos_next = self._motor_to_leg_real(motor_real_pos_next)
-        leg_simu_vel = (leg_real_pos_next - leg_real_pos) / temp_dt  # = leg_real_vel
+        motor_real_vel = motor_fb_vel * np.array([-1, 1])
+        leg_real_vel = self._dl_dm(motor_real_pos) * motor_real_vel  # dl/dt = dl/dm * dm/dt
+        leg_simu_vel = leg_real_vel
         return leg_simu_pos, leg_simu_vel
     
-    def wheel_to_motor(self, wheel_vel):
-        return np.array([wheel_vel[0] * -self._deceleration, wheel_vel[1] * self._deceleration])
+    def wheel_to_motor(self, wheel_action_vel):
+        return np.array([wheel_action_vel[0] * -self._deceleration, wheel_action_vel[1] * self._deceleration])
     
-    def motor_to_wheel(self, motor_vel):
-        return np.array([motor_vel[0] / -self._deceleration, motor_vel[1] / self._deceleration])
+    def motor_to_wheel(self, motor_fb_vel):
+        return np.array([motor_fb_vel[0] / -self._deceleration, motor_fb_vel[1] / self._deceleration])
+
+    def _m(self, l):
+        # calculate motor position from leg position
+        m = -37.77 * l ** 3 + 26.70 * l ** 2 - 1.47 * l + 0.77
+        return m
     
-    def _leg_to_motor_real(self, leg_real):
-        # fit curve
-        length = leg_real + self._leg_offset
-        return -37.77 * length ** 3 + 26.70 * length ** 2 - 1.47 * length + 0.77 - self._motor_offset
+    def _dm_dl(self, l):
+        # derivative of m over l
+        dm_dl = -37.77 * 3 * l ** 2 + 26.70 * 2 * l - 1.47
+        return dm_dl
     
-    def _motor_to_leg_real(self, motor_real):
-        # fit curve
-        angle = motor_real + self._motor_offset
-        return 0.1102 * angle ** 3 - 0.4610 * angle ** 2 + 0.848 * angle + -0.3540 - self._leg_offset
+    def _l(self, m):
+        # calculate leg position from motor
+        l = 0.1102 * m ** 3 - 0.4610 * m ** 2 + 0.848 * m + -0.3540
+        return l
+    
+    def _dl_dm(self, m):
+        # derivative of l over m
+        dl_dm = 0.1102 * 3 * m ** 2 - 0.4610 * 2 * m + 0.848
+        return dl_dm
     
     def _clip_leg(self, leg):
         return np.clip(leg, self._leg_min, self._leg_max)
