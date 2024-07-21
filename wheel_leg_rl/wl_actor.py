c8194baa45665b94e3b51691c51550ce75eb4dd3
Yao-Xinchen <yao29@illinois.edu>
2024-07-21 19:22:08 +0800

    add kalman filter to action

--- wheel_leg_rl/wl_actor.py
+++ wheel_leg_rl/wl_actor.py
@@ -5,6 +5,7 @@ import threading
 import asyncio
 from wheel_leg_rl.obs_encoder import ObsEncoder
 import rclpy
+from filterpy.kalman import KalmanFilter
 
 class WLActor:
     def __init__(self, actor_path, encoder_path, dt=0.005):
@@ -24,9 +25,16 @@ class WLActor:
         self._num_actions = 4
         self._num_latents = 3
         self._observation = np.zeros((self._num_observations,), dtype=np.float32)
-        self._action = np.zeros((self._num_actions,), dtype=np.float32)
-        self._latent = np.zeros((self._num_latents,), dtype=np.float32) # output from encoder
-        self._observation[6:9] = np.array([0.0, 0.0, 0.5])  # commands
+        self._observation[6:9] = np.array([0.0, 0.0, 0.12])  # commands
+
+        # kalman filter
+        action_filter = KalmanFilter(dim_x=4, dim_z=4)  # [l_leg_pos, r_leg_pos, l_wheel_vel, r_wheel_vel]
+        action_filter.x = np.array([0.0, 0.0, 0.0, 0.0])
+        action_filter.F = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        action_filter.H = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        action_filter.P *= 1000
+        action_filter.R *= 10
+        self._action_filter = action_filter
 
         # threading
         self._dt = dt
@@ -72,7 +80,7 @@ class WLActor:
     def output_action(self):
             '''action: [l_leg_pos, r_leg_pos, l_wheel_vel, r_wheel_vel]'''
             with self._lock:
-                return self._action
+                return self._action_filter.x
 
     async def _run(self):
         while rclpy.ok():
@@ -82,6 +90,7 @@ class WLActor:
             input_data = {self._input_name: np.concatenate((observation, latent))}
             action = self._session.run([self._output_name], input_data)[0]
             with self._lock:
-                self._action = action
+                self._action_filter.predict()
+                self._action_filter.update(action)
                 self._observation[15:] = action
             await asyncio.sleep(self._dt)
