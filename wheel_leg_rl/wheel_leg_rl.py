b59617d053a620a74df4842b6b5ea09593162730
Yao-Xinchen <yao29@illinois.edu>
2024-07-22 19:35:19 +0800

    add filter to velocity and rotate ang vel

--- wheel_leg_rl/wheel_leg_rl.py
+++ wheel_leg_rl/wheel_leg_rl.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import time
+from filterpy.kalman import KalmanFilter
 from array import array
 from wheel_leg_rl.wl_actor import WLActor
 from wheel_leg_rl.quaternion import *
@@ -51,6 +52,16 @@ class WheelLegRL(Node):
         self._action_wheel_scale = 7.0
         self._command_scale = np.array([self._lin_vel_scale, self._ang_vel_scale, self._height_scale])
 
+        # filters
+        # too much noise in motor velocity
+        vel_filter = KalmanFilter(dim_x=4, dim_z=4)
+        vel_filter.x = np.array([0.0, 0.0, 0.0, 0.0])
+        vel_filter.F = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        vel_filter.H = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        vel_filter.P *= 1000
+        vel_filter.R *= 100
+        self._vel_filter = vel_filter
+
         # timeouts
         self._timeout_threshold = 0.2
         self._last_command_moment = self.get_clock().now()
@@ -66,16 +77,24 @@ class WheelLegRL(Node):
         for id, pos, vel in zip(msg.motor_id, msg.present_pos, msg.present_vel):
             self._motor_pos[id] = pos
             self._motor_vel[id] = vel
+
         # convert to dof
         leg_motor_pos = np.array([self._motor_pos["L_LEG"], self._motor_pos["R_LEG"]])
         leg_motor_vel = np.array([self._motor_vel["L_LEG"], self._motor_vel["R_LEG"]])
         wheel_motor_vel = np.array([self._motor_vel["L_WHL"], self._motor_vel["R_WHL"]])
         leg_pos, leg_vel = self._convert.motor_to_leg(leg_motor_pos, leg_motor_vel)
         wheel_vel = self._convert.motor_to_wheel(wheel_motor_vel)
+        # self._debug_pub.publish(Float64MultiArray(data=leg_motor_vel))
 
         # input to actor
-        self._actor.input_dof_pos(np.array([leg_pos[0], leg_pos[1]]) * self._dof_pos_scale)
-        self._actor.input_dof_vel(np.array([leg_vel[0], leg_vel[1], wheel_vel[0], wheel_vel[1]]) * self._dof_vel_scale)
+        # pos
+        self._actor.input_dof_pos(leg_pos * self._dof_pos_scale)
+        # self._debug_pub.publish(Float64MultiArray(data=leg_pos))
+        # vel
+        self._vel_filter.predict()
+        self._vel_filter.update(np.concatenate((leg_vel, wheel_vel)))
+        self._actor.input_dof_vel(self._vel_filter.x * self._dof_vel_scale)
+        # self._debug_pub.publish(Float64MultiArray(data=self._vel_filter.x))
 
     def _command_callback(self, msg: Move):
         command = np.array([msg.vel_x, msg.omega, msg.height])
@@ -83,12 +102,6 @@ class WheelLegRL(Node):
         self._last_command_moment = self.get_clock().now()
 
     def _imu_callback(self, msg: Imu):
-        # angular velocity
-        x = msg.angular_velocity.x
-        y = msg.angular_velocity.y
-        z = msg.angular_velocity.z
-        self._actor.input_base_ang_vel(np.array([x, y, z]) * self._ang_vel_scale)
-
         # gravity vector
         world_gravity = np.array([0, 0, -1.0])  # unit vector pointing down
         world_to_imu = [msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z]
@@ -96,14 +109,21 @@ class WheelLegRL(Node):
         imu_gravity = quat_rotate_inverse(world_to_imu, world_gravity)
         base_gravity = quat_rotate(imu_to_base, imu_gravity)
         self._actor.input_projected_gravity(base_gravity)
+        # self._debug_pub.publish(Float64MultiArray(data=base_gravity))
+
+        # angular velocity
+        ang_vel = np.array([msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z])
+        ang_vel = quat_rotate_inverse(world_to_imu, ang_vel)
+        self._actor.input_base_ang_vel(ang_vel * self._ang_vel_scale)
+        # self._debug_pub.publish(Float64MultiArray(data=ang_vel))
 
     def _pub_callback(self):
         # get scaled action
         action = self._actor.output_action()
         leg_pos = np.array([action[0], action[1]]) * self._action_leg_scale
         wheel_vel = np.array([action[2], action[3]]) * self._action_wheel_scale
-        data = np.concatenate((leg_pos, wheel_vel))
-        self._debug_pub.publish(Float64MultiArray(data=data))
+        # data = np.concatenate((leg_pos, wheel_vel))
+        # self._debug_pub.publish(Float64MultiArray(data=data))
         # convert to motor
         leg_motor_pos = self._convert.leg_to_motor(leg_pos)
         wheel_motor_vel = self._convert.wheel_to_motor(wheel_vel)
